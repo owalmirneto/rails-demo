87bb0f1f6b7b55649546366804091676521b549d
Walmir Neto <owalmirneto@gmail.com>
2024-02-17 10:48:49 -0300

    chore: create aws instances

---
+++ tofu/aws_instances.tf
@@ -0,0 +1,15 @@
+locals {
+  instance_ami  = "ami-0c7217cdde317cfec"
+  instance_type = "t2.micro"
+  instance_tags = {
+    Name    = "Ruby on Rails"
+    Project = "Ubuntu for Rails Demo"
+  }
+}
+
+resource "aws_instance" "demo_instance" {
+  ami           = local.instance_ami
+  instance_type = local.instance_type
+
+  tags = local.instance_tags
+}
